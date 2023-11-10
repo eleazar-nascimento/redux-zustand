16c4a8a347129e34bb3b9d1deb45e8330ced1f70
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-11-09 21:09:37 -0300

    feat: ensure that the lesson is booked as selected and that the module and lesson are changed.

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,20 +1,31 @@
-import { Video } from "lucide-react";
+import { PlayCircle, Video } from "lucide-react";
 
 type LessonProps = {
   title: string;
   duration: string;
+  isCurrent?: boolean;
   onPlay: () => void
 }
 
 export function Lesson({
   title,
   duration,
-  onPlay
+  onPlay,
+  isCurrent = false
 }: LessonProps) {
 
   return (
-    <button onClick={onPlay} className="flex items-center gap-3 text-sm text-zinc-400">
-      <Video className="w-4 h-4 text-zinc-500" />
+    <button 
+      onClick={onPlay}
+      data-active={isCurrent}
+      disabled={isCurrent}
+      className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-200 enabled:hover:text-zinc-100"
+    >
+      { isCurrent ? (
+        <PlayCircle className="w-4 h-4 text-emerald-200" /> 
+      ) : (
+        <Video className="w-4 h-4 text-zinc-500" />
+      ) }
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">{duration}</span>
     </button>
--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -17,6 +17,15 @@ export function Module({
   amountOfLessons,
 }: ModuleProps) {
   const dipatch = useDispatch();
+
+  const { currentLessonIndex, currentModuleIndex, } = useAppSelector((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state.player;
+
+    return {
+      currentLessonIndex,
+      currentModuleIndex,
+    }
+  });
   
   const lessons = useAppSelector((state) => {
     return state.player.course.modules[moduleIndex].lessons
@@ -42,9 +51,18 @@ export function Module({
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
           {
-            lessons.map((lesson, index) => {
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent = currentModuleIndex === moduleIndex && 
+              currentLessonIndex === lessonIndex
+
               return (
-                <Lesson key={lesson.id} title={lesson.title} duration={lesson.duration} onPlay={() => dipatch(play([moduleIndex, index]))} />
+                <Lesson 
+                  key={lesson.id} 
+                  title={lesson.title} 
+                  duration={lesson.duration} 
+                  onPlay={() => dipatch(play([moduleIndex, lessonIndex]))} 
+                  isCurrent={isCurrent}
+                />
               )
             })
           }
